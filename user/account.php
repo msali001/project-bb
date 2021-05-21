49bcf3c4eeb660d205b90e92010b566d599bb508
msali001 <musthu190@gmail.com>
2021-05-21 22:06:07 +0530

    Removed blood units dummy page

--- user/account.php
+++ user/account.php
@@ -141,9 +141,9 @@ $MySQLi_CON->close();
 							  <a class="btn btn-primary" href="bloodrequest" style="width:200px; padding-left:-10px;">Send Request</a>
 							  <a class="btn btn-primary" href="viewrequests" style="width:200px; padding-left:-10px;">View Requests</a>
 							  </div>
-                <div class="mbr-section-btn">
+                <!-- <div class="mbr-section-btn">
   							  <a class="btn btn-primary" href="bloodavailable" style="width:200px; padding-left:-10px;">Blood Units</a>
-                </div>
+                </div> -->
                         </div>
 
                     </div>
